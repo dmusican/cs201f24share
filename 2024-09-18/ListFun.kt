c04e00ec914f80bd412034d807c58dc3fde5ad9d
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-18 10:33:04 -0500

    Scripted auto-commit on change (2024-09-18 10:33:04) by gitwatch.sh

--- 2024-09-18/ListFun.kt
+++ 2024-09-18/ListFun.kt
@@ -9,6 +9,12 @@ fun main()
     val morethings = mutableListOf(19, 62, 128, 34)
     morethings[0] = 17
     println(morethings)
+
+    val startEmpty = mutableListOf<Int>()
+    startEmpty.add(19)
+    startEmpty.add(22)
+    startEmpty[1] = 57
+    println(startEmpty)
 }
 
  
