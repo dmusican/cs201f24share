0bbf70bb8ea927631c61c2dddd4972ff31db7425
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-18 10:21:52 -0500

    Scripted auto-commit on change (2024-09-18 10:21:52) by gitwatch.sh

--- 2024-09-18/GuessingGame.kt
+++ 2024-09-18/GuessingGame.kt
@@ -3,19 +3,22 @@ fun main()
     var secret = 47
     var guess = -1
 
-    print("What is your guess? ")
-    guess = readln().toInt()
-    if (guess > secret)
+    while (guess != secret)
     {
-        println("Too big")
-    }
-    else if (guess < secret)
-    {
-        println("Too small")
-    }
-    else
-    {
-        println("You got it")
+        print("What is your guess? ")
+        guess = readln().toInt()
+        if (guess > secret)
+        {
+            println("Too big")
+        }
+        else if (guess < secret)
+        {
+            println("Too small")
+        }
+        else
+        {
+            println("You got it")
+        }
     }
 }
 
