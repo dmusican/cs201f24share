cbde969863d6ed38a6e1bd5c39d3aeb9fcc3c533
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-18 10:45:16 -0500

    Scripted auto-commit on change (2024-09-18 10:45:16) by gitwatch.sh

---
+++ 2024-09-18/Adding.kt
@@ -0,0 +1,15 @@
+fun addItUp(listToAdd)
+{
+    var total = 0
+    for (number in listToAdd)
+    {
+        total = number + total
+    }
+}
+
+fun main()
+{
+    val list1 = listOf(9, 2, 3, 5, 7)
+    val total = addItUp(list1)
+
+}
