23471b1fe86491b43ac29a1d3c907f2a868f22e1
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-20 10:06:59 -0500

    Scripted auto-commit on change (2024-09-20 10:06:59) by gitwatch.sh

--- 2024-09-20/Counter.kt
+++ 2024-09-20/Counter.kt
@@ -1,3 +1,15 @@
+class Counter {
+    // Instance variables
+    var value = 0
+
+    fun click() {
+        value = value + 1
+    }
+
+    fun reset() {
+        value = 0
+    }
+}
 fun main() {
     val mycounter = Counter()
     mycounter.reset()
