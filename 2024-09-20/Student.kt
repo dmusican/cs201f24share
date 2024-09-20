1079303d6e81dca300eb0c3a7d75cd7afcc1fecf
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-20 10:26:50 -0500

    Scripted auto-commit on change (2024-09-20 10:26:50) by gitwatch.sh

--- 2024-09-20/Student.kt
+++ 2024-09-20/Student.kt
@@ -1,5 +1,7 @@
 class Student(var name: String,
               var age: Int) {
+    
+    var gradStatus = null
  
     fun doBirthday() {
         age = age + 1
@@ -9,6 +11,7 @@ class Student(var name: String,
 fun main() {
     val s1 = Student("Dave", 20)
     val s2 = Student("Schiller", 140)
+    s1.gradStatus = "SO05" // soph 5th term
     s1.doBirthday()
     println(s1.age)
 }
