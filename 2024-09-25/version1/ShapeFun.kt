0f6d016ac22d8a5233ab1a579ba3bff5432c49ca
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-25 09:58:39 -0500

    Scripted auto-commit on change (2024-09-25 09:58:39) by gitwatch.sh

--- 2024-09-25/version1/ShapeFun.kt
+++ 2024-09-25/version1/ShapeFun.kt
@@ -7,3 +7,12 @@ class Rectangle(var length: Double,
         return 2*length + 2*width
     }
 }
+
+class Circle(var radius: Double) {
+    fun area(): Double {
+        return Math.PI * radius * radius
+    }
+    fun perimeter(): Double {
+        return 2 * Math.PI * radius
+    }
+}
