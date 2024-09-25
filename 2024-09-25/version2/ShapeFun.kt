2acd3c629551c183687363ee25762c2f61233d93
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-09-25 10:35:40 -0500

    Scripted auto-commit on change (2024-09-25 10:35:40) by gitwatch.sh

---
+++ 2024-09-25/version2/ShapeFun.kt
@@ -0,0 +1,39 @@
+class Rectangle(var length: Double,
+                var width: Double) {
+    fun area(): Double {
+        return length * width
+    }
+    fun perimeter(): Double {
+        return 2*length + 2*width
+    }
+}
+
+class Circle(var radius: Double) {
+    fun area(): Double {
+        return Math.PI * radius * radius
+    }
+    fun perimeter(): Double {
+        return 2 * Math.PI * radius
+    }
+}
+
+fun main() {
+    print("(r)ectangle or (c)ircle? ")
+    val choice = readln()
+    var s:Any? = null
+    if (choice == "r") {
+        s = Rectangle(3.2, 6.7)
+    } else {
+        s = Circle(6.1)
+    }
+    println(s.area())
+    println(s.perimeter())
+}
+
+
+
+
+
+
+
+
