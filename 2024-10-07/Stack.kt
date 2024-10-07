9d35e50df0f73173569db751d17c7d0edb961a42
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-07 10:07:33 -0500

    Scripted auto-commit on change (2024-10-07 10:07:33) by gitwatch.sh

--- 2024-10-07/Stack.kt
+++ 2024-10-07/Stack.kt
@@ -1,58 +1,13 @@
-interface Stack {
-    fun push(item: Int)
-    fun pop(): Int
-    fun peek(): Int
+interface Stack<T> {
+    fun push(item: T)
+    fun pop(): T
+    fun peek(): T
     fun isEmpty(): Boolean
+
 }
 
-class ArrayStack : Stack {
-    // Initial array of size 10
-    private var array = arrayOfNulls<Int>(10)
-    // Number of items in stack
-    private var stackSize = 0
-
-    override fun isEmpty(): Boolean {
-        return stackSize == 0
-    }
-
-    override fun push(item: Int) {
-        if (stackSize >= array.count()) {
-            val newArray =
-                arrayOfNulls<Int>(array.count()*2)
-            // Copy contents of old array
-            for (i in array.indices) {
-                newArray[i] = array[i]
-            }
-            array = newArray
-        }
-        array[stackSize] = item
-        stackSize++
-    }
-
-    override fun pop(): Int {
-        val itemOnTop = peek()
-        array[stackSize-1] = null
-        stackSize--
-        return itemOnTop
-    }
-
-    override fun peek(): Int {
-        if (stackSize == 0) {
-            throw RuntimeException("Stack is empty!")
-        }
-        // I know, based on my code above, that below
-        // can't be null, and if it is, it is a bug in my
-        // code, and I want to see that error happen
-        return array[stackSize-1]!!
-    }
-}
-
-fun main() {
-    val myStack = ArrayStack()
-    myStack.push(3)
-    myStack.push(70)
-    myStack.push(18)
-    println(myStack.peek())
-    myStack.pop()
-    println(myStack.peek())
-}
+class LinkedStack<T> : Stack<T> {
+    private data class Node<T>(
+        var item: T,
+        var next: Node<T>?
+    )
