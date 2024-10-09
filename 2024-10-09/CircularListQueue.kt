c2f2224d9cf33f80f29e0d6df1d5bc448f654e6a
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-09 10:42:01 -0500

    Scripted auto-commit on change (2024-10-09 10:42:01) by gitwatch.sh

--- 2024-10-09/CircularListQueue.kt
+++ 2024-10-09/CircularListQueue.kt
@@ -15,6 +15,12 @@ class CircularListQueue<T> {
             rear = 0
         }
 
+        // Queue is empty, but list is there
+        else if (front == -1) {
+            front = 0
+            rear = 0
+            list[0] = item
+        }
 
         if (rear < list.count()) {
             list[rear] = item
