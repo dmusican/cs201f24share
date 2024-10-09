e92a8c8d281ee8cdec659f280c76927665c31a92
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-09 10:39:05 -0500

    Scripted auto-commit on change (2024-10-09 10:39:05) by gitwatch.sh

---
+++ 2024-10-09/CircularListQueue.kt
@@ -0,0 +1,24 @@
+class ListQueue<T> {
+    private var list = mutableListOf<T>()
+    private var front = -1
+    private var rear = -1
+
+    fun isEmpty(): Boolean {
+        return (front == -1)
+    }
+
+    fun enqueue(item: T) {
+        // Empty queue?
+        if (front == -1) {
+            front = 0
+            rear = 0
+        } else {
+            rear = rear + 1
+        }
+
+        if (rear < list.count()) {
+            list[rear] = item
+        } else {
+            list.add(item)
+        }
+    }
