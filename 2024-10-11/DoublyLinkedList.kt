52078fbb0596d59b4898c116345a68bc767561ee
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-11 10:10:35 -0500

    Scripted auto-commit on change (2024-10-11 10:10:35) by gitwatch.sh

--- 2024-10-11/DoublyLinkedList.kt
+++ 2024-10-11/DoublyLinkedList.kt
@@ -8,3 +8,22 @@ class DoublyLinkedList<T> {
     private var tail: Node<T>? = null
 
     fun insertAtBeginning(item: T) {
+        val newNode = Node(item, null, null)
+
+        if (head == null) {
+            head = newNode
+            tail = newNode
+        } else {
+            head!!.prev = newNode
+            newNode.next = head
+            head = newNode
+        }
+    }
+}
+
+fun main() {
+    val mylist = DoublyLinkedList<Int>()
+    mylist.insertAtBeginning(1)
+    mylist.insertAtBeginning(2)
+    mylist.insertAtBeginning(3)
+}
