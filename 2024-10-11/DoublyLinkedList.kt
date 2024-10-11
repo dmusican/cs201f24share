2c40f404d036abf60f63a2cdceaff7c0d8a64bf7
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-11 09:50:13 -0500

    Scripted auto-commit on change (2024-10-11 09:50:13) by gitwatch.sh

--- 2024-10-11/DoublyLinkedList.kt
+++ 2024-10-11/DoublyLinkedList.kt
@@ -3,4 +3,6 @@ data class Node<T> (
     var next: Node<T>?,
     var prev: Node<T>?)
 
-class DoublyLinkedList<T>
+class DoublyLinkedList<T> {
+    private var head: Node<T> = null
+    private var tail: Node<T> = null
