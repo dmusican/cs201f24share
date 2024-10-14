230f59b8a63dd8669955556de2393e0a499aaca5
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-14 10:34:59 -0500

    Scripted auto-commit on change (2024-10-14 10:34:59) by gitwatch.sh

--- 2024-10-14/MapDemo.kt
+++ 2024-10-14/MapDemo.kt
@@ -2,7 +2,8 @@ fun main() {
     // val myMap = mutableMapOf("one" to 1)
     val myMap = mutableMapOf<String, Int>()
     myMap.put("one", 1)
-    myMap["two"] = 2
+    myMap["twothousand"] = 2000
+    myMap["three"] = 3
     println(myMap.get("one"))
     println(myMap["two"])
     println(myMap["three"])
