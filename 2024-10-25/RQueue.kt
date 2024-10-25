77d504d0b25bb428bdb648d825700920c7bd762c
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-25 10:18:22 -0500

    Scripted auto-commit on change (2024-10-25 10:18:22) by gitwatch.sh

--- 2024-10-25/RQueue.kt
+++ 2024-10-25/RQueue.kt
@@ -1,4 +1,8 @@
 // Structurally recursive queue
+// Completely empty: all are null
+// One item: it's in front
+// Two items: front and rear (inside is null)
+// More than that....
 class RQueue<T> {
     var front: T? = null
     var rear: T? = null
