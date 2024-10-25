4a435ecee2479e180f018fcbc6f5ba1d38d3e13f
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-25 09:50:21 -0500

    Scripted auto-commit on change (2024-10-25 09:50:21) by gitwatch.sh

--- 2024-10-25/Fact.kt
+++ 2024-10-25/Fact.kt
@@ -2,3 +2,12 @@
 // 5!  = 5 * 4 * 3 * 2 * 1
 
 fun fact(n: Int): Int {
+    // Base case
+    if (n == 1) {
+        return 1
+    } else {
+        // Recursive case
+        // wrong code, but deep belief
+        return n * fact(n-1)
+    }
+}
