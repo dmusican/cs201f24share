187ea0617946ecd67b5fd995ed245bd25176cbd7
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-25 10:16:15 -0500

    Scripted auto-commit on change (2024-10-25 10:16:15) by gitwatch.sh

---
+++ 2024-10-25/RQueue.kt
@@ -0,0 +1,13 @@
+// Structurally recursive queue
+class RQueue<T> {
+    var front: T? = null
+    var rear: T? = null
+    var inside: RQueue<T>? = null
+
+    fun enqueue(value: T) {
+
+    }
+
+    override fun toString(): String {
+
+    }
