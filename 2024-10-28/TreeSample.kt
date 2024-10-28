b90aeda011a9612b31369c795bcd86c988abc1fe
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-28 10:05:14 -0500

    Scripted auto-commit on change (2024-10-28 10:05:14) by gitwatch.sh

--- 2024-10-28/TreeSample.kt
+++ 2024-10-28/TreeSample.kt
@@ -1,6 +1,6 @@
 // Binary Tree: tree where each node has a maximum of two children
 data class TreeNode<T>(
-    var data: T
+    var data: T,
     var leftChild: TreeNode<T>? = null,
     var rightChild: TreeNode<T?> = null)
 )
