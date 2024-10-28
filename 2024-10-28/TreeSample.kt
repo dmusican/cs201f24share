016be666b157fa714c95ebd6ce8539a027d93228
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-28 10:08:04 -0500

    Scripted auto-commit on change (2024-10-28 10:08:04) by gitwatch.sh

--- 2024-10-28/TreeSample.kt
+++ 2024-10-28/TreeSample.kt
@@ -3,6 +3,9 @@ data class TreeNode<T>(
     var data: T,
     var leftChild: TreeNode<T>? = null,
     var rightChild: TreeNode<T>? = null)
-)
 
 fun main() {
+    val root = TreeNode("allyb")
+    root.leftChild = TreeNode("dean livingston")
+    root.rightChild = TreeNode("provost mattson")
+    root.leftChild.leftChild = TreeNode("dave musicant")
