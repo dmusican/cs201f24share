b86ad6d7547da6a18f4412305e08ea2852e0c6e8
Dave Musicant (limbo) <dmusicant@carleton.edu>
2024-10-29 08:24:21 -0500

    trie in progress

--- code-for-readings/src/main/kotlin/in-progress/Trie.kt
+++ code-for-readings/src/main/kotlin/in-progress/Trie.kt
@@ -1,11 +1,58 @@
 data class TrieNode(
-    var children: Array<TrieNode?> = Array<TrieNode?>(26) { null },
+    var childNode: Array<TrieNode?> = Array<TrieNode?>(26) { null },
 
     // This will keep track of number of strings that
     // are stored in the Trie from root node to any Trie
     // node.
     var wordCount: Int = 0
 )
+
+fun insert(root: TrieNode, key: String) {
+    // Initialize the currentNode pointer with the root node
+    var currentNode = root
+
+    // Note that letter is a 'Char' type
+    for (letter in key) {
+        val index = letter - 'a'
+
+        // Check if the node exist for the current character in the Trie.
+        if (currentNode.childNode[index] == null) {
+
+            // Keep the reference for the newly created node.
+            currentNode.childNode[index] = TrieNode()
+        }
+
+        // Now, move the current node pointer to the newly created node.
+        currentNode = currentNode.childNode[index]!!
+    }
+
+    // Increment the wordEndCount for the last currentNode pointer this implies
+    // that there is a string ending at currentNode.
+    currentNode.wordCount++
+}
+
+fun isPrefixExist(root: TrieNode, key: String): Boolean {
+    // Initialize the currentNode pointer with the root node
+    var currentNode = root
+
+    // Iterate across the length of the string
+    for (c in key) {
+
+        // Check if the node exists for the current character in the Trie.
+        if (currentNode.childNode[c - 'a'] == null) {
+
+            // Given word as a prefix does not exist in Trie
+            return false
+        }
+
+        // Move the currentNode pointer to the already
+        // existing node for current character.
+        currentNode = currentNode.childNode[c - 'a']!!
+    }
+
+      // Prefix exists in the Trie
+    return true
+}
 
 fun main() {
     println(TrieNode())
