0faedd79cce95d6ef7a8abab871d2bcd98daaebe
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-29 10:33:27 -0500

    Scripted auto-commit on change (2024-10-29 10:33:27) by gitwatch.sh

--- code-for-readings/src/main/kotlin/in-progress/Trie.kt
+++ code-for-readings/src/main/kotlin/in-progress/Trie.kt
@@ -76,7 +76,60 @@ fun search(root: TrieNode, key: String): Boolean {
     return (currentNode.wordCount > 0)
 }
 
+fun deleteKey(root: TrieNode, word: String): Boolean {
+        var currentNode = root
+        var lastBranchNode: TrieNode? = null
+        var lastBranchChar = 'a'
 
+        for (char c : word.toCharArray()) {
+            if (currentNode.childNode[c - 'a'] == null) {
+                // If the current node has no child, the word is not present
+                return false;
+            } else {
+                int count = 0;
+                // Count the number of non-null child nodes
+                for (int i = 0; i < 26; i++) {
+                    if (currentNode.childNode[i] != null)
+                        count++;
+                }
+
+                if (count > 1) {
+                    // If there are more than one child, store the last branch information
+                    lastBranchNode = currentNode;
+                    lastBranchChar = c;
+                }
+                currentNode = currentNode.childNode[c - 'a'];
+            }
+        }
+
+        int count = 0;
+        // Count the number of non-null child nodes at the last character
+        for (int i = 0; i < 26; i++) {
+            if (currentNode.childNode[i] != null)
+                count++;
+        }
+
+        // Case 1: The deleted word is a prefix of other words in Trie.
+        if (count > 0) {
+            // Decrement the word count and indicate successful deletion
+            currentNode.wordCount--;
+            return true;
+        }
+
+        // Case 2: The deleted word shares a common prefix with other words in Trie.
+        if (lastBranchNode != null) {
+            // Remove the link to the deleted word
+            lastBranchNode.childNode[lastBranchChar - 'a'] = null;
+            return true;
+        }
+        // Case 3: The deleted word does not share any common prefix with other words in Trie.
+        else {
+            // Remove the link to the deleted word from the root
+            root.childNode[word.charAt(0) - 'a'] = null;
+            return true;
+        }
+    }
+}
 
 fun main() {
     println(TrieNode())
