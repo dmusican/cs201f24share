c8bc7020f759cb45effe34abe4b30bc32af98dc6
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-10-30 10:53:40 -0500

    Scripted auto-commit on change (2024-10-30 10:53:40) by gitwatch.sh

--- 2024-10-30/BSTSet.kt
+++ 2024-10-30/BSTSet.kt
@@ -2,3 +2,18 @@ class BSTSet<T> {
     data class Node<T>(var key :T
                        var left: Node<T>? = null,
                        var right: Node<T>? = null)
+
+    var root: Node<T>? = null
+
+    // insert: start at root
+    // if value needs to go left, insert into left subtree
+    // if value needs to ro right, insert into right subtree
+
+    fun insert(item:T) {
+        root = insert(item, root)
+    }
+
+    private fun insert(item: T, subtree: Node<T>?): Node<T> {
+        if (subtree == null) {
+            return Node<T>(item)
+        }
