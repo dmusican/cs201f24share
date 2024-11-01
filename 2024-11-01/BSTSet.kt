fab5ec8f6ce99feb1834ab13e4e8855949d8a6ff
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-01 09:57:37 -0500

    Scripted auto-commit on change (2024-11-01 09:57:37) by gitwatch.sh

---
+++ 2024-11-01/BSTSet.kt
@@ -0,0 +1,6 @@
+class BSTSet<T: Comparable<T>> {
+    data class Node<T>(var key: T,
+                       var left: Node<T>? = null,
+                       var right: Node<T>? = null)
+
+    private var root: Node<T>? = null
