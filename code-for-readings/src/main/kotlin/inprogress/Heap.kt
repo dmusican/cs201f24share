455fa910a11a08b1353f8682f24b729fde417ab9
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-04 13:44:32 -0600

    Scripted auto-commit on change (2024-11-04 13:44:32) by gitwatch.sh

--- code-for-readings/src/main/kotlin/inprogress/Heap.kt
+++ code-for-readings/src/main/kotlin/inprogress/Heap.kt
@@ -4,82 +4,74 @@
 
 class Heap {
     void heapify(ArrayList<Integer> hT, int i) {
-        int size = hT.size();
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
+        int size = hT.size()
+        int largest = i
+        int l = 2 * i + 1
+        int r = 2 * i + 2
         if (l < size && hT.get(l) > hT.get(largest))
-            largest = l;
+            largest = l
         if (r < size && hT.get(r) > hT.get(largest))
-            largest = r;
+            largest = r
 
         if (largest != i) {
-            int temp = hT.get(largest);
-            hT.set(largest, hT.get(i));
-            hT.set(i, temp);
+            int temp = hT.get(largest)
+            hT.set(largest, hT.get(i))
+            hT.set(i, temp)
 
-            heapify(hT, largest);
+            heapify(hT, largest)
         }
     }
 
     void insert(ArrayList<Integer> hT, int newNum) {
-        int size = hT.size();
+        int size = hT.size()
         if (size == 0) {
-            hT.add(newNum);
+            hT.add(newNum)
         } else {
-            hT.add(newNum);
-            for (int i = size / 2 - 1; i >= 0; i--) {
-                heapify(hT, i);
+            hT.add(newNum)
+            for (int i = size / 2 - 1 i >= 0 i--) {
+                heapify(hT, i)
             }
         }
     }
 
     void deleteNode(ArrayList<Integer> hT, int num)
     {
-        int size = hT.size();
-        int i;
-        for (i = 0; i < size; i++)
+        int size = hT.size()
+        int i
+        for (i = 0 i < size i++)
         {
             if (num == hT.get(i))
-                break;
+                break
         }
 
-        int temp = hT.get(i);
-        hT.set(i, hT.get(size-1));
-        hT.set(size-1, temp);
+        int temp = hT.get(i)
+        hT.set(i, hT.get(size-1))
+        hT.set(size-1, temp)
 
-        hT.remove(size-1);
-        for (int j = size / 2 - 1; j >= 0; j--)
+        hT.remove(size-1)
+        for (int j = size / 2 - 1 j >= 0 j--)
         {
-            heapify(hT, j);
+            heapify(hT, j)
         }
-    }
-
-    void printArray(ArrayList<Integer> array, int size) {
-        for (Integer i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
     }
 
 }
 
 fun main() {
 
-    ArrayList<Integer> array = new ArrayList<Integer>();
-    int size = array.size();
+    val list = mutableListOf<Int>()
 
-    Heap h = new Heap();
-    h.insert(array, 3);
-    h.insert(array, 4);
-    h.insert(array, 9);
-    h.insert(array, 5);
-    h.insert(array, 2);
+    val h = Heap()
+    h.insert(list, 3)
+    h.insert(list, 4)
+    h.insert(list, 9)
+    h.insert(list, 5)
+    h.insert(list, 2)
 
-    System.out.println("Max-Heap array: ");
-    h.printArray(array, size);
+    println("Max-Heap array: ")
+    println(list)
 
-    h.deleteNode(array, 4);
-    System.out.println("After deleting an element: ");
-    h.printArray(array, size);
+    h.deleteNode(list, 4)
+    println("After deleting an element: ")
+    println(list)
 }
