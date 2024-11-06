60d40a0edff5589bf99f55d2676a20df3ceb9df4
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-06 11:37:28 -0600

    Scripted auto-commit on change (2024-11-06 11:37:28) by gitwatch.sh

--- code-for-readings/src/main/kotlin/inprogress/Mergesort.kt
+++ code-for-readings/src/main/kotlin/inprogress/Mergesort.kt
@@ -1,32 +1,32 @@
-// Mergesort code
+// Mergesort codfse
 // Ported to Kotlin by Dave Musicant from Java code at
 // https://opendsa-server.cs.vt.edu/OpenDSA/Books/Everything/html/MergesortImpl.html
 
 fun <T> mergesort(A: MutableList<Comparable<T>>, temp: MutableList<Comparable<T>>, left: Int, right: Int) {
-  if (left == right) {        // List has one record
-      return
-  }
-  val mid = (left+right)/2          // Select midpoint
-  mergesort(A, temp, left, mid)     // Mergesort first half
-  mergesort(A, temp, mid+1, right)  // Mergesort second half
-  for (i in left..right) {    // Copy subarray to temp
-    temp[i] = A[i]
-  }
-  // Do the merge operation back to A
-  var i1 = left
-  var i2 = mid + 1
-  for (curr in left..right) {
-    if (i1 == mid+1) {                 // Left sublist exhausted
-      A[curr] = temp[i2++]
+    if (left == right) {        // List has one record
+        return
     }
-    else if (i2 > right) {             // Right sublist exhausted
-      A[curr] = temp[i1++]
+    val mid = (left+right)/2          // Select midpoint
+    mergesort(A, temp, left, mid)     // Mergesort first half
+    mergesort(A, temp, mid+1, right)  // Mergesort second half
+    for (i in left..right) {    // Copy subarray to temp
+        temp[i] = A[i]
     }
-    else if (temp[i1] < temp[i2]) {     // Get smaller value
-      A[curr] = temp[i1++]
+    // Do the merge operation back to A
+    var i1 = left
+    var i2 = mid + 1
+    for (curr in left..right) {
+        if (i1 == mid+1) {                 // Left sublist exhausted
+            A[curr] = temp[i2++]
+        }
+        else if (i2 > right) {             // Right sublist exhausted
+            A[curr] = temp[i1++]
+        }
+        else if (temp[i1] < temp[i2]) {     // Get smaller value
+            A[curr] = temp[i1++]
+        }
+        else{
+            A[curr] = temp[i2++]
+        }
     }
-    else{
-      A[curr] = temp[i2++]
-    }
-  }
 }
