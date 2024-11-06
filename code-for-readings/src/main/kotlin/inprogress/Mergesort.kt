e2d861018a8b589363daaf5ff0ba60aa29cdf6c3
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-06 11:39:54 -0600

    Scripted auto-commit on change (2024-11-06 11:39:54) by gitwatch.sh

--- code-for-readings/src/main/kotlin/inprogress/Mergesort.kt
+++ code-for-readings/src/main/kotlin/inprogress/Mergesort.kt
@@ -2,13 +2,15 @@
 // Ported to Kotlin by Dave Musicant from Java code at
 // https://opendsa-server.cs.vt.edu/OpenDSA/Books/Everything/html/MergesortImpl.html
 
-fun <T> mergesort(A: MutableList<Comparable<T>>, temp: MutableList<Comparable<T>>, left: Int, right: Int) {
+// fun <T> mergesort(A: MutableList<Comparable<T>>) {
+//     _mergesort(A, listOf<
+fun <T> _mergesort(A: MutableList<Comparable<T>>, temp: MutableList<Comparable<T>>, left: Int, right: Int) {
     if (left == right) {        // List has one record
         return
     }
     val mid = (left+right)/2          // Select midpoint
-    mergesort(A, temp, left, mid)     // Mergesort first half
-    mergesort(A, temp, mid+1, right)  // Mergesort second half
+    _mergesort(A, temp, left, mid)     // Mergesort first half
+    _mergesort(A, temp, mid+1, right)  // Mergesort second half
     for (i in left..right) {    // Copy subarray to temp
         temp[i] = A[i]
     }
