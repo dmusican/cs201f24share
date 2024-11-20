92a4936bfa7fde0c34df3b0b8b4cdea9169c4898
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-20 10:12:24 -0600

    Scripted auto-commit on change (2024-11-20 10:12:24) by gitwatch.sh

--- 2024-11-20/Demo3.kt
+++ 2024-11-20/Demo3.kt
@@ -4,4 +4,8 @@ fun main() {
     // I want to have 0, 5, 10, 15, 20, 25, ...
     var list = MutableList<Int>(10, {index -> index * 5})
     println(list)
+
+    // Shorter
+    var list = MutableList<Int>(10, {it * 5})
+    println(list)
 }
