6b7e249164f030167988bc9d40cc2505b8bbd3eb
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-20 10:18:18 -0600

    Scripted auto-commit on change (2024-11-20 10:18:18) by gitwatch.sh

--- 2024-11-20/Demo4.kt
+++ 2024-11-20/Demo4.kt
@@ -2,11 +2,13 @@ class Thing {
     var word: String? = null
 
     fun countit(): Int {
-        if (word != null) {
-            return word.count()
-        } else {
-            return 0
-        }
+        // if (word != null) {
+        //     return word.count()
+        // } else {
+        //     return 0
+        // }
+
+        return word?.let {it.count()} ?: 0
     }
 }
 
