7093ec66df2901d2e32bf275b08484729b3ce148
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-20 10:02:03 -0600

    Scripted auto-commit on change (2024-11-20 10:02:03) by gitwatch.sh

--- 2024-11-20/Demo2.kt
+++ 2024-11-20/Demo2.kt
@@ -3,14 +3,10 @@ data class Person(
     val age: Int
 )
 
-fun getAge(person: Person): Int {
-    return person.age
-}
-
 fun main() {
     val people = listOf(Person("Alice", 29),
                         Person("Bob", 31))
     // print out the oldest person
-    val result = people.maxByOrNull(::getAge)
+    val result = people.maxByOrNull( {person: Person -> person.age} )
     print(result)
 }
