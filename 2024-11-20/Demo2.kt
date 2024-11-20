8ae181a1e98ae1974b7f567f078ee70167341952
Dave Musicant (expanse) <dmusicant@carleton.edu>
2024-11-20 10:01:01 -0600

    Scripted auto-commit on change (2024-11-20 10:01:01) by gitwatch.sh

---
+++ 2024-11-20/Demo2.kt
@@ -0,0 +1,16 @@
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+fun getAge(person: Person): Int {
+    return person.age
+}
+
+fun main() {
+    val people = listOf(Person("Alice", 29),
+                        Person("Bob", 31))
+    // print out the oldest person
+    val result = people.maxByOrNull(::getAge)
+    print(result)
+}
